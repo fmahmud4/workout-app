e26358f387241be0f648a887ba1b8b388ce62aec
Fayaz <fayaz@marginedge.com>
2021-10-28 20:31:53 -0400

    initial project

---
+++ app/src/main/java/edu/gmu/cs477/project2_fmahmud4/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.gmu.cs477.project2_fmahmud4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
